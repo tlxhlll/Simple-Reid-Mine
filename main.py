ffe7af4364f0c68cd7ad9b13b2a2a06b87e98273
tjc1411 <1599480206@qq.com>
2022-02-20 13:15:24 +0800

    attr

--- main.py
+++ main.py
@@ -301,6 +301,7 @@ def get_test_loader(dataset, height, width, batch_size, workers, testset=None):
         Preprocessor(testset, root=dataset.images_dir, transform=test_transformer),
         batch_size=batch_size, num_workers=workers,
         shuffle=False, pin_memory=True)
+    print("dataset.attr={}\n".format(getattr(dataset)))
 
     return test_loader
 
