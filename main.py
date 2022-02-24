7ca23de4636202e7f0727b20310d40fccdceaad0
tjc1411 <1599480206@qq.com>
2022-02-24 10:51:15 +0800

    new

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def main(config):
                 features, labels = extract_features(model, cluster_loader, print_freq=50)
                 features = torch.cat([features[f].unsqueeze(0) for f, _, _ in sorted(dataset.train)], 0)
                 labels = torch.cat([labels[f].unsqueeze(0) for f, _, _ in sorted(dataset.train)], 0)
-                print("feature cat size={}/n".format(features.size()))
+                print("feature cat size={}/n".format(labels.size()))
         embed()
         start_train_time = time.time()
         train(epoch, model, classifier, criterion_cla, criterion_pair, optimizer, trainloader)
